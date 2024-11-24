6c031fc891c4d494c0fb05e335df52037eeab03d
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-11-24 12:45:51 +0530

    update app.js with set function to receive message from child

--- day07/src/App.js
+++ day07/src/App.js
@@ -4,11 +4,15 @@ import Childcom from "./Childcom";
 
 function App() {
   const [childname, setChildname] = useState("Default");
+  const receiveName = (name) => {
+    setChildname(name);
+  }
 
   return (
     <div className="App">
       <h2>Hello React I'm Parent</h2>
-      <Childcom />
+      <Childcom setfun={receiveName}/>
+      <p>Parent: My child said: {childname}</p>
     </div>
   );
 }

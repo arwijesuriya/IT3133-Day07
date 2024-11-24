7134ee8f8571ee7424175779c3c05a0965cb5e07
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-11-24 12:43:17 +0530

    setup the child and parent

--- day07/src/App.js
+++ day07/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import { useState } from "react";
+import Childcom from "./Childcom";
 
 function App() {
   const [childname, setChildname] = useState("Default");
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Hello React I'm Parent</h2>
+      <Childcom />
+    </div>
+  );
 }
 
 export default App;
